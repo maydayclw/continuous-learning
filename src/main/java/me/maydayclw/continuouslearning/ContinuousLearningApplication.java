403ff5d15aea8ead1cc9780226c8e9055bdee446
maydayclw <maydayclw@foxmail.com>
2018-12-10 15:07:43 +0800

    Initial commit

---
+++ src/main/java/me/maydayclw/continuouslearning/ContinuousLearningApplication.java
@@ -0,0 +1,12 @@
+package me.maydayclw.continuouslearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContinuousLearningApplication.class, args);
+    }
+}
---
+++ src/test/java/me/maydayclw/continuouslearning/ContinuousLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package me.maydayclw.continuouslearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContinuousLearningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
